cb1ae0d148f6e3dd3dd88baacda243aced3884b1
Peter Broadbent <103116958+PetyrB2@users.noreply.github.com>
2022-11-16 12:27:55 +0000

    Initial Push

---
+++ src/FileInput/FileInputFun.java
@@ -0,0 +1,24 @@
+package FileInput;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileInputFun {
+
+	public static void main(String[] args) {
+		
+		Scanner infile;
+		
+		try {
+			infile = new Scanner(new File('input.txt'));
+			int input;
+			
+			while()
+			
+		}//end try
+		catch(FileNotFoundException ex) {
+			
+		}//end catch
+	}//end main
+
+}
